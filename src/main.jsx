10dc5bd330c3fb737eb14b420e21c70ec0f9f883
Facundo Elias <facundo.elias10@gmail.com>
2026-01-27 16:47:22 +0100

    feat: add global error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import GlobalErrorBoundary from '@/components/ui/GlobalErrorBoundary';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GlobalErrorBoundary>
+      <App />
+    </GlobalErrorBoundary>
   </StrictMode>,
 )
