7340e130bcc4f778f870ad0c1e3b155285535995
Facundo Elias <facundo.elias10@gmail.com>
2026-01-27 16:58:21 +0100

    feat: setup seo and vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import GlobalErrorBoundary from '@/components/ui/GlobalErrorBoundary';
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GlobalErrorBoundary>
       <App />
+      <Analytics />
     </GlobalErrorBoundary>
   </StrictMode>,
 )
